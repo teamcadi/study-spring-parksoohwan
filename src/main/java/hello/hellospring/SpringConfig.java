1e1dac3195126435bd4b6348302d1b409e04b611
Soohwan Park <vividswan@naver.com>
2020-09-19 02:01:01 +0900

    Update SpringConfig.java

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
     @Bean
     MemberService memberService(){
         return new MemberService(memberRepository());
